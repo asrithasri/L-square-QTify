fb5dd9dcba3ff74b8f1dee91957746f98cd61825
asrithasri <asrithk133@gmail.com>
2024-07-27 20:19:10 +0530

    Add logo.png

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-import styles from "./Logo.module.css"
+import styles from "./Logo.module.css";
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
-        <img src={LogoImage} alt="logo" width={67} />;
+        <img src={LogoImage} alt="logo" width={67} />
     </div>
-  )
+  );
 }
